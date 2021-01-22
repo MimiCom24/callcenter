a536dfe5ef18704f6cdd903ba838669fd9246e8d
slavo3dev <slavo@slavo3.com>
2021-01-22 06:18:20 -0500

    server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.get("test", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Slavo3 Call Center");
 });
 
