c0db28c544c624e35156aa07a00d1b7b6c3a1a18
slavo3dev <slavo@slavo3.com>
2021-01-22 06:23:55 -0500

    creating simple server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
   res.send("Slavo3 Call Center");
 });
 
+// receive phone number
+app.get("/login", (req, res) => {});
+
+// verify phone number
+app.get("/verify", (res, req) => {});
 app.listen(PORT, () => {
   console.log("Server is Running on ", PORT);
 });
