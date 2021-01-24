fb75dc3a3e6442c6e86be309487ab5927c0cb804
slavo3dev <slavo@slavo3.com>
2021-01-24 07:07:42 -0500

    adding login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
+import Login from "./components/Login";
+
 function App() {
   return (
     <div className="App">
-      <h1>Call Center</h1>
+      <Login />
     </div>
   );
 }
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Grid, Header, Form, Segment } from "semantic-ui-react";
+function Login() {
+  return (
+    <Grid textAlign="center" verticalAlign="middle" style={{ height: "100vh" }}>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="red" textAlign="center">
+          Login in to your account
+        </Header>
+        <Form>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="username"
+            ></Form.Input>
+            <Form.Input
+              fluid
+              icon="mobile alternate"
+              iconPosition="left"
+              placeholder="mobile number"
+            ></Form.Input>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  );
+}
+
+export default Login;
